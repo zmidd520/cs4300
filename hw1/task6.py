775a1b3226826df60d08659fff849f14fe1d087c
Zach Middleton <zach.middleton12@yahoo.com>
2025-02-18 10:05:44 +0000

    done with task 6

--- hw1/task6.py
+++ hw1/task6.py
@@ -1,13 +1,11 @@
-def count_words(filename):
-    with open(filename, 'r') as f:
-        content = f.read().split()
+def count_words(file):
+    # read file contents
+    content = file.read().split()
 
-        # get length of lines
-        numWords = len(content)
+    # get word count and return it
+    numWords = len(content)
 
     return numWords
 
-#words = count_words("task6_read_me.txt")
-#print(words)
 
     
--- hw1/tests/test_task6.py
+++ hw1/tests/test_task6.py
@@ -4,20 +4,28 @@ import task6
 import os
 
 # create function based on filename
-def create_function(name):
+def create_function(file, wordCount):
+    name = file[:-4]
 
     # create dynamic test function 
-    exec(f"""def test_{name}(filename):
-    words = task6.count_words(filename)
-    assert words == len(filename.read().split())
+    exec(f"""def test_{name}():      
+    with open(\"{file}\", 'r') as f:       
+        words = task6.count_words(f)
+        assert words == {wordCount}
     """)
 
     return locals()[f"test_{name}"]
-"""
+
 def test_task6():
     for file in os.listdir():
         if ".txt" in file:
-            name = file[:-4]
-            func = create_function(name)
-            func(file)
-"""
+
+            # get the word count for the file locally
+            with open(file, 'r') as f:
+                wcount = len(f.read().split())
+
+            # generate and call dynamic function
+            func = create_function(file, wcount)
+            func()
+
+test_task6()
